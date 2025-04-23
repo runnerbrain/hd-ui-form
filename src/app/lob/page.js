fccfb4d03268b3d467675ddbb7ba791a107ae88c
Mahmoud Badreddine <runnerbrain@gmail.com>
2025-04-23 12:42:11 -0400

    Make src/app/lob/page.js dynamic to support useSearchParams

--- src/app/lob/page.js
+++ src/app/lob/page.js
@@ -1,4 +1,6 @@
 'use client';
+export const dynamic = 'force-dynamic';
+
 import {useState} from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import commonStyles from '../common/common.module.css';
